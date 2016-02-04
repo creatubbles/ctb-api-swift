3587cdcd4d15770f4558a1c25c602d2f142358b7
Michal Miedlarz <m.miedlarz@nomtek.com>
2016-02-04 11:54:58 +0100

    Initial Commit

---
+++ CreatubblesAPIClient/CreatubblesAPIClient.h
@@ -0,0 +1,19 @@
+//
+//  CreatubblesAPIClient.h
+//  CreatubblesAPIClient
+//
+//  Created by Michal Miedlarz on 04.02.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CreatubblesAPIClient.
+FOUNDATION_EXPORT double CreatubblesAPIClientVersionNumber;
+
+//! Project version string for CreatubblesAPIClient.
+FOUNDATION_EXPORT const unsigned char CreatubblesAPIClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CreatubblesAPIClient/PublicHeader.h>
+
+
