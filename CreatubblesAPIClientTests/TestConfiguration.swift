57d58dba6866863bd27245ca6597ca7d54fe2bb7
Michal Miedlarz <m.miedlarz@nomtek.com>
2016-02-05 15:39:12 +0100

    Added AppId and AppSecret to tests.

---
+++ CreatubblesAPIClientTests/TestConfiguration.swift
@@ -0,0 +1,15 @@
+//
+//  TestConfiguration.swift
+//  CreatubblesAPIClient
+//
+//  Created by Michal Miedlarz on 05.02.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+import UIKit
+
+class TestConfiguration: NSObject
+{
+    static let appId = "TEST_APP_ID"
+    static let appSecret = "TEST_APP_SECRET"
+}
