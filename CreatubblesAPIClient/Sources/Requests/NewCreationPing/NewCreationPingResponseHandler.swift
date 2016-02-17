0be42bd38ab32a917c015ec4ed69ef87ccca395d
Michal Miedlarz <m.miedlarz@nomtek.com>
2016-02-17 09:34:00 +0100

    [CI-71] Added NewCreationPingRequest.

---
+++ CreatubblesAPIClient/Sources/Requests/NewCreationPing/NewCreationPingResponseHandler.swift
@@ -0,0 +1,14 @@
+//
+//  NewCreationPingResponseHandler.swift
+//  CreatubblesAPIClient
+//
+//  Created by Michal Miedlarz on 17.02.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+import UIKit
+
+class NewCreationPingResponseHandler: ResponseHandler
+{
+
+}
--- CreatubblesAPIClientTests/RequestSpec.swift
+++ CreatubblesAPIClientTests/RequestSpec.swift
@@ -401,5 +401,31 @@ class RequestSpec: QuickSpec
                 }
             }
         }
+        
+        describe("Creation ping request")
+        {
+            it("Should have proper method")
+            {
+                let request = NewCreationPingRequest(uploadId: "12345")
+                expect(request.method).to(equal(RequestMethod.PUT))
+            }
+            
+            it("Should have proper endpoint")
+            {
+                let uploadId = "12345"
+                let request = NewCreationPingRequest(uploadId: uploadId)
+                expect(request.endpoint).to(equal("uploads/"+uploadId))
+            }
+            
+            it("Should have proper parameters")
+            {
+                let abortError = "AbortError"
+                let successfulRequest = NewCreationPingRequest(uploadId: "12345")
+                let failedRequest = NewCreationPingRequest(uploadId: "12345", abortError: abortError)
+                expect(successfulRequest.parameters).to(beEmpty())
+                expect(failedRequest.parameters["aborted_with"] as? String).to(equal(abortError))
+            }
+            
+        }
     }
 }
