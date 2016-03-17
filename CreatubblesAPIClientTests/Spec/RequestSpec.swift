d5e3905571f4a074dddeba675c16c5cbe2b30f29
Jakub Jankowski <kubajankowski90@gmail.com>
2016-03-17 15:37:29 +0100

    [CI-91] Added tests to NewCommentRequest

--- CreatubblesAPIClientTests/Spec/RequestSpec.swift
+++ CreatubblesAPIClientTests/Spec/RequestSpec.swift
@@ -703,5 +703,45 @@ class RequestSpec: QuickSpec
                 expect(CommentsRequest(userId: identifier).endpoint).to(equal("users/\(identifier)/comments"))
             }
         }
+        describe("New comment request")
+        {
+            it("Should have a proper method")
+            {
+                let newCommentData = NewCommentData(creationId: "", text: "That's the best comment you've ever read!")
+                let request = NewCommentRequest(data: newCommentData)
+                expect(request.method).to(equal(RequestMethod.POST))
+            }
+            it("Should have a correct endpoint when commenting on a creation")
+            {
+                let id = "identifier"
+                let newCommentData = NewCommentData(creationId: id, text: "That's the best comment you've ever read!")
+                let request = NewCommentRequest(data: newCommentData)
+                expect(request.endpoint).to(equal("creations/\(id)/comments"))
+            }
+            it("Should have a correct endpoint when commenting on a gallery")
+            {
+                let id = "identifier"
+                let newCommentData = NewCommentData(galleryId: id, text: "That's the best comment you've ever read!")
+                let request = NewCommentRequest(data: newCommentData)
+                expect(request.endpoint).to(equal("galleries/\(id)/comments"))
+            }
+            it("Should have a correct endpoint when commenting on a user profile")
+            {
+                let id = "identifier"
+                let newCommentData = NewCommentData(userId: id, text: "That's the best comment you've ever read!")
+                let request = NewCommentRequest(data: newCommentData)
+                expect(request.endpoint).to(equal("users/\(id)/comments"))
+            }
+            it("Should have correct parameters when commenting on a creation")
+            {
+                let identifier = "identifier"
+                let text = "That's actually not a smart comment."
+                
+                let newCommentRequest = NewCommentData(creationId: identifier, text: text)
+                let request = NewCommentRequest(data: newCommentRequest)
+                expect(request.parameters["creation_id"] as? String).to(equal(identifier))
+                expect(request.parameters["text"] as? String).to(equal(text))
+            }
+        }
     }
 }
