03e766191e53f2409c8b0d30bfb9d2ffaf9ee5ae
Michal Miedlarz <m.miedlarz@nomtek.com>
2016-06-03 22:10:43 +0200

    [CI-119] Divider Request and ResponseHandler specs to domain-only specs

---
+++ CreatubblesAPIClientTests/Spec/Comment/NewCommentRequestSpec.swift
@@ -0,0 +1,57 @@
+//
+//  NewCommentRequestSpec.swift
+//  CreatubblesAPIClient
+//
+//  Created by Michal Miedlarz on 03.06.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import CreatubblesAPIClient
+
+class NewCommentRequestSpec: QuickSpec
+{
+    override func spec()
+    {
+        describe("New comment request")
+        {
+            it("Should have a proper method")
+            {
+                let newCommentData = NewCommentData(creationId: "", text: "That's the best comment you've ever read!")
+                let request = NewCommentRequest(data: newCommentData)
+                expect(request.method).to(equal(RequestMethod.POST))
+            }
+            it("Should have a correct endpoint when commenting on a creation")
+            {
+                let id = "identifier"
+                let newCommentData = NewCommentData(creationId: id, text: "That's the best comment you've ever read!")
+                let request = NewCommentRequest(data: newCommentData)
+                expect(request.endpoint).to(equal("creations/\(id)/comments"))
+            }
+            it("Should have a correct endpoint when commenting on a gallery")
+            {
+                let id = "identifier"
+                let newCommentData = NewCommentData(galleryId: id, text: "That's the best comment you've ever read!")
+                let request = NewCommentRequest(data: newCommentData)
+                expect(request.endpoint).to(equal("galleries/\(id)/comments"))
+            }
+            it("Should have a correct endpoint when commenting on a user profile")
+            {
+                let id = "identifier"
+                let newCommentData = NewCommentData(userId: id, text: "That's the best comment you've ever read!")
+                let request = NewCommentRequest(data: newCommentData)
+                expect(request.endpoint).to(equal("users/\(id)/comments"))
+            }
+            it("Should have correct parameters when commenting on a creation")
+            {
+                let identifier = "identifier"
+                let text = "That's actually not a smart comment."
+                
+                let newCommentRequest = NewCommentData(creationId: identifier, text: text)
+                let request = NewCommentRequest(data: newCommentRequest)
+                expect(request.parameters["text"] as? String).to(equal(text))
+            }
+        }
+    }
+}
---
+++ CreatubblesAPIClientTests/Spec/Creation/CreationResponseHandlerSpec.swift
@@ -0,0 +1,127 @@
+//
+//  CreationResponseHandlerSpec.swift
+//  CreatubblesAPIClient
+//
+//  Created by Michal Miedlarz on 03.06.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+import Foundation
+
+import Quick
+import Nimble
+@testable import CreatubblesAPIClient
+
+class CreationResponseHandlerSpec: QuickSpec
+{
+    override func spec()
+    {
+        describe("Fetch Creations response handler")
+        {
+            it("Should return correct value for creations after login")
+            {
+                let request = FetchCreationsRequest(page: 1, perPage: 10, galleryId: nil, userId: nil, sort: .Recent, keyword: nil)
+                let sender =  TestComponentsFactory.requestSender
+                waitUntil(timeout: 10)
+                {
+                    done in
+                    sender.login(TestConfiguration.username, password: TestConfiguration.password)
+                    {
+                        (error: ErrorType?) -> Void in
+                        expect(error).to(beNil())
+                        sender.send(request, withResponseHandler:FetchCreationsResponseHandler
+                            {
+                                (creations: Array<Creation>?,pageInfo: PagingInfo?, error: ErrorType?) -> Void in
+                                expect(creations).notTo(beNil())
+                                expect(error).to(beNil())
+                                expect(pageInfo).notTo(beNil())
+                                sender.logout()
+                                done()
+                            })
+                    }
+                }
+            }
+            
+            it("Should return correct value for recommended creations after login")
+            {
+                guard let creationId = TestConfiguration.testCreationIdentifier
+                    else { return }
+                
+                let request = FetchCreationsRequest(page: 1, perPage: 10, recommendedCreationId: creationId)
+                let sender =  TestComponentsFactory.requestSender
+                waitUntil(timeout: 10)
+                {
+                    done in
+                    sender.login(TestConfiguration.username, password: TestConfiguration.password)
+                    {
+                        (error: ErrorType?) -> Void in
+                        expect(error).to(beNil())
+                        sender.send(request, withResponseHandler:FetchCreationsResponseHandler
+                            {
+                                (creations: Array<Creation>?,pageInfo: PagingInfo?, error: ErrorType?) -> Void in
+                                expect(creations).notTo(beNil())
+                                expect(error).to(beNil())
+                                expect(pageInfo).notTo(beNil())
+                                sender.logout()
+                                done()
+                            })
+                    }
+                }
+            }
+            
+            it("Should return correct value for recommended creations based by user identifier after login")
+            {
+                guard let userId = TestConfiguration.testUserIdentifier
+                    else { return }
+                
+                let request = FetchCreationsRequest(page: 1, perPage: 10, recommendedUserId: userId)
+                let sender =  TestComponentsFactory.requestSender
+                waitUntil(timeout: 10)
+                {
+                    done in
+                    sender.login(TestConfiguration.username, password: TestConfiguration.password)
+                    {
+                        (error: ErrorType?) -> Void in
+                        expect(error).to(beNil())
+                        sender.send(request, withResponseHandler:FetchCreationsResponseHandler
+                            {
+                                (creations: Array<Creation>?,pageInfo: PagingInfo?, error: ErrorType?) -> Void in
+                                expect(creations).notTo(beNil())
+                                expect(error).to(beNil())
+                                expect(pageInfo).notTo(beNil())
+                                sender.logout()
+                                done()
+                            })
+                    }
+                }
+            }
+            
+            it("Should return a correct value for single creation after login")
+            {
+                guard let creationId = TestConfiguration.testCreationIdentifier
+                    else { return }
+                
+                let request = FetchCreationsRequest(creationId: creationId)
+                let sender =  TestComponentsFactory.requestSender
+                waitUntil(timeout: 10)
+                {
+                    done in
+                    sender.login(TestConfiguration.username, password: TestConfiguration.password)
+                    {
+                        (error: ErrorType?) -> Void in
+                        expect(error).to(beNil())
+                        sender.send(request, withResponseHandler:FetchCreationsResponseHandler
+                            {
+                                (creations: Array<Creation>?, pageInfo: PagingInfo?, error: ErrorType?) -> Void in
+                                expect(creations).notTo(beNil())
+                                expect(error).to(beNil())
+                                expect(pageInfo).notTo(beNil())
+                                sender.logout()
+                                done()
+                            })
+                    }
+                }
+            }
+        }
+    }
+}
