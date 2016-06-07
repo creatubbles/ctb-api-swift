3e3887348cc60585b8eed2bdcd954f8767ba1277
Michal Miedlarz <m.miedlarz@nomtek.com>
2016-06-07 09:53:25 +0200

    [CI-120] Added GallerySubmission and Mapper.

---
+++ CreatubblesAPIClient/Sources/Mapper/GallerySubmissionMapper.swift
@@ -0,0 +1,36 @@
+//
+//  GallerySubmissionMapper.swift
+//  CreatubblesAPIClient
+//
+//  Created by Michal Miedlarz on 07.06.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+import UIKit
+import ObjectMapper
+
+class GallerySubmissionMapper: Mappable
+{
+    var identifier: String?
+    
+    var galleryIdentifier: String?
+    var creationIdentifier: String?
+    
+    var creationRelationship: RelationshipMapper?
+    var galleryRelationship:  RelationshipMapper?
+    
+    required init?(_ map: Map)
+    {
+        
+    }
+    
+    func mapping(map: Map)
+    {
+        identifier  <- map["id"]
+        galleryIdentifier  <- map["attributes.gallery_id"]
+        creationIdentifier <- map["attributes.creation_id"]
+
+        creationRelationship <- map["relationships.creation.data"]
+        galleryRelationship <- map["relationships.gallery.data"]
+    }
+}
