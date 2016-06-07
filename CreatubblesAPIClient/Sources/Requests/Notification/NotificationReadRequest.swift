9e5368efb8979e6744805537925351f511674552
Michal Miedlarz <m.miedlarz@nomtek.com>
2016-06-07 15:40:16 +0200

    [CI-120] Added read notification request.

---
+++ CreatubblesAPIClient/Sources/Requests/Notification/NotificationReadRequest.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationReadRequest.swift
+//  CreatubblesAPIClient
+//
+//  Created by Michal Miedlarz on 07.06.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+import UIKit
+
+class NotificationReadRequest: Request
+{
+    override var method: RequestMethod { return .POST }
+    override var endpoint: String { return "notifications/\(notificationIdentifier)/read" }
+    override var parameters: Dictionary<String, AnyObject> { return Dictionary<String, AnyObject>() }
+    
+    private let notificationIdentifier: String
+    
+    init(notificationIdentifier: String)
+    {
+        self.notificationIdentifier = notificationIdentifier
+    }
+    
+}
