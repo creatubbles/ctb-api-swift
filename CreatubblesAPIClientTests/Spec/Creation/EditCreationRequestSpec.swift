862f11e9db8de09b4a7144f34a2483bd3293999d
Michal Miedlarz <m.miedlarz@nomtek.com>
2016-06-15 15:16:09 +0200

    [CI-64] Added EditCreationRequest

---
+++ CreatubblesAPIClientTests/Spec/Creation/EditCreationRequestSpec.swift
@@ -0,0 +1,39 @@
+//
+//  EditCreationRequestSpec.swift
+//  CreatubblesAPIClient
+//
+//  Created by Michal Miedlarz on 08.06.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+
+import Quick
+import Nimble
+@testable import CreatubblesAPIClient
+
+class EditCreationRequestSpec: QuickSpec
+{
+    override func spec()
+    {
+        describe("EditCreationRequest")
+        {
+            it("Should use PUT method")
+            {
+                let request = EditCreationRequest(identifier: "", data: EditCreationData())
+                expect(request.method).to(equal(RequestMethod.PUT))
+            }
+            
+            it("Should use 'creations/IDENTIFIER endpoint")
+            {
+                let identifier = "TestIdentifier"
+                let request = EditCreationRequest(identifier: identifier, data: EditCreationData())
+                expect(request.endpoint).to(equal("creations/\(identifier)"))
+            }
+            
+            it("Should return proper parameters")
+            {
+            
+            }
+        }
+    }
+}
