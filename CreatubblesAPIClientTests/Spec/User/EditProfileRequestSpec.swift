a6e4671aeb9c8bb82c0b719c4c61006f7b1cc893
Michal Miedlarz <m.miedlarz@nomtek.com>
2016-06-16 10:11:44 +0200

    [CI-121] Added EditProfileRequest and ResponseHandler

---
+++ CreatubblesAPIClientTests/Spec/User/EditProfileRequestSpec.swift
@@ -0,0 +1,38 @@
+//
+//  EditProfileRequestSpec.swift
+//  CreatubblesAPIClient
+//
+//  Created by Michal Miedlarz on 16.06.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import CreatubblesAPIClient
+
+class EditProfileRequestSpec: QuickSpec
+{
+    override func spec()
+    {
+        describe("EditProfileRequest")
+        {
+            it("Should have proper method")
+            {
+                let request = EditProfileRequest(identifier: "", data: EditProfileData())
+                expect(request.method).to(equal(RequestMethod.PUT))
+            }
+            
+            it("Should have `users/IDENTIFIER/account` endpoint")
+            {
+                let identifier = "TestUserIdentifier"
+                let request = EditProfileRequest(identifier: identifier, data: EditProfileData())
+                expect(request.endpoint).to(equal("users/\(identifier)/account"))
+            }
+            
+            it("Should parse parameters properly")
+            {
+                //TODO
+            }
+        }
+    }
+}
