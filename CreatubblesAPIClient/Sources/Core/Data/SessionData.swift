4a3275ecb28db0a0039d72220bb6db6b9f033890
Dawid Płatek <dawid@inspace.io>
2016-06-30 14:01:29 +0200

    CRI-191 - add ability to easily retrieve or set session tokens

---
+++ CreatubblesAPIClient/Sources/Core/Data/SessionData.swift
@@ -0,0 +1,23 @@
+//
+//  SessionData.swift
+//  CreatubblesAPIClient
+//
+//  Created by Dawid Płatek on 30.06.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+import UIKit
+
+@objc public class SessionData: NSObject {
+    let accessToken: String?
+    let idToken: String?
+    let accessTokenExpiry: NSDate?
+    let refreshToken: String?
+    
+    init(accessToken: String?, idToken: String?, accessTokenExpiry: NSDate?, refreshToken: String?) {
+        self.accessToken = accessToken
+        self.idToken = idToken
+        self.accessTokenExpiry = accessTokenExpiry
+        self.refreshToken = refreshToken
+    }
+}
