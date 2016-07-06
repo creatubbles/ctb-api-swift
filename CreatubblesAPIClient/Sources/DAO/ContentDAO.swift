c2553b9164bf428bee7d7e891ba57939ae387c69
Jakub Jankowski <kubajankowski90@gmail.com>
2016-07-06 13:01:45 +0200

    [CI-125] fixed a typo, kicked podspec version

--- CreatubblesAPIClient/Sources/DAO/ContentDAO.swift
+++ CreatubblesAPIClient/Sources/DAO/ContentDAO.swift
@@ -38,7 +38,7 @@ class ContentDAO: NSObject
         return requestSender.send(request, withResponseHandler: handler)
     }
     
-    func getMyConnectionsContetn(pagingData: PagingData?,  completion: ContentEntryClosure?) -> RequestHandler
+    func getMyConnectionsContent(pagingData: PagingData?,  completion: ContentEntryClosure?) -> RequestHandler
     {
         let request = ContentRequest(type: .Connected, page: pagingData?.page, perPage: pagingData?.pageSize)
         let handler = ContentResponseHandler(completion: completion)
