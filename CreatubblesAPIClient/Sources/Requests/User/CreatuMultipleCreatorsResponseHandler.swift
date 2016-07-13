e396d2489787daf002e02d1e7b3fe53258c02c4c
Jakub Jankowski <kubajankowski90@gmail.com>
2016-07-13 15:50:48 +0200

    [CI-128] Added CreateMultipleCreators request, added request's test

---
+++ CreatubblesAPIClient/Sources/Requests/User/CreatuMultipleCreatorsResponseHandler.swift
@@ -0,0 +1,14 @@
+//
+//  CreatuMultipleCreatorsResponseHandler.swift
+//  CreatubblesAPIClient
+//
+//  Created by Nomtek on 13.07.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+import UIKit
+
+class CreatuMultipleCreatorsResponseHandler: ErrorResponseHandler
+{
+    
+}
