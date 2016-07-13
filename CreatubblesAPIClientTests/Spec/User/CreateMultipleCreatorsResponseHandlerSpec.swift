3cc904b96c1f879e6bf38db42509818f7aeced43
Jakub Jankowski <kubajankowski90@gmail.com>
2016-07-13 15:54:39 +0200

    [CI-128] Fixes for request's tests

---
+++ CreatubblesAPIClientTests/Spec/User/CreateMultipleCreatorsResponseHandlerSpec.swift
@@ -0,0 +1,13 @@
+//
+//  CreateMultipleCreatorsResponseHandlerSpec.swift
+//  CreatubblesAPIClient
+//
+//  Created by Nomtek on 13.07.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+import UIKit
+
+class CreateMultipleCreatorsResponseHandlerSpec: NSObject {
+
+}
