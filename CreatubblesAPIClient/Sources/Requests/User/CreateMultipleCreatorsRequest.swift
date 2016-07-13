2f5f48a96938e1ae79c7e7e0653730bbeafabe7d
Jakub Jankowski <kubajankowski90@gmail.com>
2016-07-13 15:50:48 +0200

    [CI-128] Added CreateMultipleCreators request, added request's test

---
+++ CreatubblesAPIClient/Sources/Requests/User/CreateMultipleCreatorsRequest.swift
@@ -0,0 +1,42 @@
+//
+//  CreateMultipleCreatorsRequest.swift
+//  CreatubblesAPIClient
+//
+//  Created by Nomtek on 13.07.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+import UIKit
+
+class CreateMultipleCreatorsRequest: Request
+{
+    override var method: RequestMethod  { return .POST }
+    override var endpoint: String       { return "creator_builder_jobs" }
+    override var parameters: Dictionary<String, AnyObject> { return prepareParameters() }
+    
+    private let amount: Int
+    private let birthYear: Int
+    private let group: String?
+
+    init(amount: Int, birthYear:Int, group: String?)
+    {
+        self.amount = amount
+        self.birthYear = birthYear
+        self.group = group
+    }
+
+    private func prepareParameters() -> Dictionary<String, AnyObject>
+    {
+        var params = Dictionary<String, AnyObject>()
+        
+        params["amount"] = amount
+        params["birthYear"] = birthYear
+        
+        if let group = group
+        {
+            params["group"] = group
+        }
+        
+        return params
+    }
+}
---
+++ CreatubblesAPIClientTests/Spec/User/CreateMultipleCreatorsRequestSpec.swift
@@ -0,0 +1,73 @@
+//
+//  CreateMultipleCreatorsRequestSpec.swift
+//  CreatubblesAPIClient
+//
+//  Created by Nomtek on 13.07.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import CreatubblesAPIClient
+
+class CreateMultipleCreatorsRequestSpec: QuickSpec
+{
+    override func spec()
+    {
+        describe("New creator request")
+        {
+            let amount = 15
+            let birthYear = 2000
+            let group: Group = nil
+
+            var multipleCreatorsRequest: CreateMultipleCreatorsRequest
+            {
+                return CreateMultipleCreatorsRequest(amount, birthYear: birthYear, group: group?)
+            }
+            
+            it("Should have proper endpoint")
+            {
+                let request = multipleCreatorsRequest
+                expect(request.endpoint).to(equal("creator_builder_jobs"))
+            }
+            
+            it("Should have proper method")
+            {
+                let request = multipleCreatorsRequest
+                expect(request.method).to(equal(RequestMethod.POST))
+            }
+            
+            it("Should have proper parameters")
+            {
+                let request = creatorRequest
+                let params = request.parameters
+                
+                expect(params["amount"] as? Int).to(equal(amount))
+                expect(params["birth_year"] as? Int).to(equal(birthYear))
+                expect(params["group"] as Group).to(equal(group))
+            }
+            
+            it("Should return correct value after login")
+            {
+                let sender = TestComponentsFactory.requestSender
+                waitUntil(timeout: 10)
+                {
+                    done in
+                    sender.login(TestConfiguration.username, password: TestConfiguration.password)
+                    {
+                        (error: ErrorType?) -> Void in
+                        expect(error).to(beNil())
+                        sender.send(multipleCreatorsRequest, withResponseHandler: DummyResponseHandler()
+                        {
+                            (response, error) -> Void in
+                            expect(response).notTo(beNil())
+                            expect(error).to(beNil())
+                            sender.logout()
+                            done()
+                        })
+                    }
+                }
+            }
+        }
+    }
+}
