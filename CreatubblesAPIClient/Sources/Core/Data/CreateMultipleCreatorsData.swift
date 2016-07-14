9b6f526379ce69926227a033a8516c44ada8b5c5
Jakub Jankowski <kubajankowski90@gmail.com>
2016-07-14 10:53:41 +0200

    [CI-128] Added api interface method

---
+++ CreatubblesAPIClient/Sources/Core/Data/CreateMultipleCreatorsData.swift
@@ -0,0 +1,13 @@
+//
+//  CreateMultipleCreatorsData.swift
+//  CreatubblesAPIClient
+//
+//  Created by Nomtek on 14.07.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+import UIKit
+
+class CreateMultipleCreatorsData: NSObject {
+
+}
