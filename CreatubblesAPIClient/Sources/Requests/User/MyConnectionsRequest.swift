563d8aefd6868ce333886a26db8927de8acfb476
Jakub Jankowski <kubajankowski90@gmail.com>
2016-07-15 14:53:44 +0200

    [CI-130] In progress

--- CreatubblesAPIClient/Sources/Requests/User/MyConnectionsRequest.swift
+++ CreatubblesAPIClient/Sources/Requests/User/MyConnectionsRequest.swift
@@ -11,16 +11,24 @@ import UIKit
 class MyConnectionsRequest: Request
 {
     override var method: RequestMethod  { return .GET }
-    override var endpoint: String       { return "users/me/connected_users" }
+//    override var endpoint: String       { return "users/me/connected_users" }
+    override var endpoint: String
+    {
+        let user = userId != nil ? userId! : "me"
+        return "users/"+user+"/connected_users"
+    }
     override var parameters: Dictionary<String, AnyObject> { return prepareParameters() }
     
     private let page: Int?
     private let perPage: Int?
     
-    init(page: Int? = nil, perPage: Int? = nil)
+    private let userId: String?
+    
+    init(page: Int? = nil, perPage: Int? = nil, userId: String? = nil)
     {
         self.page = page
         self.perPage = perPage
+        self.userId = userId
     }
     
     private func prepareParameters() -> Dictionary<String, AnyObject>
@@ -35,7 +43,10 @@ class MyConnectionsRequest: Request
         {
             params["per_page"] = perPage
         }
-        
+        if let userId = userId
+        {
+            params["user_id"] = userId
+        }
         return params
     }
     
