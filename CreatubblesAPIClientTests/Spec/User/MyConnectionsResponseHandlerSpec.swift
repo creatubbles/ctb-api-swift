d598d004655d8daa3ac7049ba8509b480ce06852
Jakub Jankowski <kubajankowski90@gmail.com>
2016-07-15 16:16:01 +0200

    [CI-130] Added tests for response handler

--- CreatubblesAPIClientTests/Spec/User/MyConnectionsResponseHandlerSpec.swift
+++ CreatubblesAPIClientTests/Spec/User/MyConnectionsResponseHandlerSpec.swift
@@ -57,6 +57,56 @@ class MyConnectionsResponseHandlerSpec: QuickSpec
                         })
                 }
             }
+            
+            it("Should return error for other user's my connections when not logged in")
+            {
+                let sender = TestComponentsFactory.requestSender
+                sender.logout()
+                
+                let page = 1
+                let perPage = 10
+                let userId = TestConfiguration.testUserIdentifier
+                waitUntil(timeout: 10)
+                {
+                    done in
+                    sender.send(MyConnectionsRequest(page: page, perPage: perPage, userId: userId), withResponseHandler:
+                        MyConnectionsResponseHandler()
+                        {
+                            (users: Array<User>?, pageInfo: PagingInfo?, error: ErrorType?) -> Void in
+                            expect(error).notTo(beNil())
+                            expect(users).to(beNil())
+                            expect(pageInfo).to(beNil())
+                            done()
+                        })
+                }
+            }
+            
+            it("Should return correct value for other user's my connections after login")
+            {
+                let sender = TestComponentsFactory.requestSender
+                let page = 1
+                let perPage = 10
+                let userId = TestConfiguration.testUserIdentifier
+                
+                waitUntil(timeout: 10)
+                {
+                    done in
+                    sender.login(TestConfiguration.username, password: TestConfiguration.password)
+                    {
+                        (error: ErrorType?) -> Void in
+                        expect(error).to(beNil())
+                        sender.send(MyConnectionsRequest(page: page, perPage: perPage, userId: userId), withResponseHandler:
+                            MyConnectionsResponseHandler()
+                            {
+                                (users: Array<User>?,pageInfo: PagingInfo?, error: ErrorType?) -> Void in
+                                expect(error).to(beNil())
+                                expect(users).notTo(beNil())
+                                expect(pageInfo).notTo(beNil())
+                                done()
+                            })
+                    }
+                }
+            }
         }
     }
 }
