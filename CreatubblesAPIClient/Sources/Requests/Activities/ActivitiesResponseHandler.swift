25cce93d0fe89af7b28bbeaac32dd9eeca170158
Dawid Płatek <dawid@inspace.io>
2016-07-22 16:31:57 +0200

    CRI-237 - handle new activities endpoint

---
+++ CreatubblesAPIClient/Sources/Requests/Activities/ActivitiesResponseHandler.swift
@@ -0,0 +1,31 @@
+//
+//  ActivitiesResponseHandler.swift
+//  CreatubblesAPIClient
+//
+//  Created by Dawid Płatek on 22.07.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+import UIKit
+import ObjectMapper
+
+class ActivitiesResponseHandler: ResponseHandler {
+    private let completion: ActivitiesClosure?
+    
+    init(completion: ActivitiesClosure?) {
+        self.completion = completion
+    }
+    
+    override func handleResponse(response: Dictionary<String, AnyObject>?, error: ErrorType?) {
+        if  let response = response, let mappers = Mapper<ActivityMapper>().mapArray(response["data"]) {
+            let metadata = MappingUtils.metadataFromResponse(response)
+            let pageInfo = MappingUtils.pagingInfoFromResponse(response)
+            let dataMapper = MappingUtils.dataIncludeMapperFromResponse(response, metadata: metadata)
+            let activities = mappers.map({ Activity(mapper: $0, dataMapper: dataMapper, metadata: metadata) })
+            
+            executeOnMainQueue { self.completion?(activities, pageInfo, ErrorTransformer.errorFromResponse(response, error: error)) }
+        } else {
+            executeOnMainQueue { self.completion?(nil, nil, ErrorTransformer.errorFromResponse(response, error: error)) }
+        }
+    }
+}
