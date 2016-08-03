6fb3eb0d9e8770f92df609e823c5b0245baa576c
Jakub Jankowski <kubajankowski90@gmail.com>
2016-08-03 12:57:28 +0200

    [CI-136] Added Partner Applications request, model and mapper.

---
+++ CreatubblesAPIClient/Sources/Requests/Partner Applications/PartnerApplicationsRequest.swift
@@ -0,0 +1,32 @@
+//
+//  PartnerApplicationsRequest.swift
+//  CreatubblesAPIClient
+//
+//  Created by Nomtek on 02.08.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+import UIKit
+
+class PartnerApplicationsRequest: Request
+{
+    override var method: RequestMethod   { return .GET }
+    override var endpoint: String        { return "partner_applications/\(id)" }
+    override var parameters: Dictionary<String, AnyObject> { return prepareParameters() }
+    
+    private let id: String
+    
+    init(id: String)
+    {
+        self.id = id
+    }
+    
+    func prepareParameters() -> Dictionary<String, AnyObject>
+    {
+        var params = Dictionary<String, AnyObject>()
+
+        params["id"] = id
+        return params
+    }
+
+}
---
+++ CreatubblesAPIClient/Sources/Requests/Partner Applications/PartnerApplicationsResponseHandler.swift
@@ -0,0 +1,37 @@
+//
+//  PartnerApplicationsResponseHandler.swift
+//  CreatubblesAPIClient
+//
+//  Created by Nomtek on 02.08.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+import UIKit
+import ObjectMapper
+
+class PartnerApplicationsResponseHandler: ResponseHandler
+{
+    private let completion: UsersClosure?
+    init(completion: UsersClosure?)
+    {
+        self.completion = completion
+    }
+    
+    override func handleResponse(response: Dictionary<String, AnyObject>?, error: ErrorType?)
+    {
+        if  let response = response,
+            let usersMapper = Mapper<UserMapper>().mapArray(response["data"])
+        {
+            let metadata = MappingUtils.metadataFromResponse(response)
+            let pageInfo = MappingUtils.pagingInfoFromResponse(response)
+            let dataMapper = MappingUtils.dataIncludeMapperFromResponse(response, metadata: metadata)
+            let users = usersMapper.map({ User(mapper: $0, dataMapper: dataMapper, metadata: metadata)})
+            
+            executeOnMainQueue { self.completion?(users, pageInfo, ErrorTransformer.errorFromResponse(response, error: error)) }
+        }
+        else
+        {
+            executeOnMainQueue { self.completion?(nil, nil, ErrorTransformer.errorFromResponse(response, error: error)) }
+        }
+    }
+}
