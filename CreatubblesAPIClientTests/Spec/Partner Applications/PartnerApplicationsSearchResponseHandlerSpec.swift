357d67fb7e0957571c35f9e2a1a071c61f143e31
Jakub Jankowski <kubajankowski90@gmail.com>
2016-08-04 16:11:05 +0200

    [CI-136] Disabled partner applications tests

--- CreatubblesAPIClientTests/Spec/Partner Applications/PartnerApplicationsSearchResponseHandlerSpec.swift
+++ CreatubblesAPIClientTests/Spec/Partner Applications/PartnerApplicationsSearchResponseHandlerSpec.swift
@@ -12,49 +12,51 @@ import Nimble
 
 class PartnerApplicationsSearchResponseHandlerSpec: QuickSpec
 {
-    let query = "example-app"
+    //MARK: - Tests disabled until Partner applications are ready on the api side
     
-    override func spec()
-    {
-        describe("Partner Applications Search response handler")
-        {
-            it("Should return correct value after login")
-            {
-                let sender = TestComponentsFactory.requestSender
-                waitUntil(timeout: 10)
-                {
-                    done in
-                    sender.login(TestConfiguration.username, password: TestConfiguration.password)
-                    {
-                        (error: ErrorType?) -> Void in
-                        expect(error).to(beNil())
-                        sender.send(PartnerApplicationsSearchRequest(query: self.query), withResponseHandler: PartnerApplicationsSearchResponseHandler()
-                        {
-                            (partnerApplications: Array<PartnerApplication>?, error: ErrorType?) -> Void in
-                            expect(error).to(beNil())
-                            expect(partnerApplications).notTo(beNil())
-                            done()
-                        })
-                    }
-                }
-            }
-            
-            it("Should return error when not logged in")
-            {
-                let sender = TestComponentsFactory.requestSender
-                sender.logout()
-                waitUntil(timeout: 10)
-                {
-                    done in
-                    sender.send(PartnerApplicationsSearchRequest(query: self.query), withResponseHandler:PartnerApplicationsSearchResponseHandler()
-                    {
-                        (partnerApplications: Array<PartnerApplication>?, error: ErrorType?) -> Void in
-                        expect(error).notTo(beNil())
-                        expect(partnerApplications).to(beNil())
-                        done()
-                    })
-                }
-            }
-        }
-    }
+//    let query = "example-app"
+//    
+//    override func spec()
+//    {
+//        describe("Partner Applications Search response handler")
+//        {
+//            it("Should return correct value after login")
+//            {
+//                let sender = TestComponentsFactory.requestSender
+//                waitUntil(timeout: 10)
+//                {
+//                    done in
+//                    sender.login(TestConfiguration.username, password: TestConfiguration.password)
+//                    {
+//                        (error: ErrorType?) -> Void in
+//                        expect(error).to(beNil())
+//                        sender.send(PartnerApplicationsSearchRequest(query: self.query), withResponseHandler: PartnerApplicationsSearchResponseHandler()
+//                        {
+//                            (partnerApplications: Array<PartnerApplication>?, error: ErrorType?) -> Void in
+//                            expect(error).to(beNil())
+//                            expect(partnerApplications).notTo(beNil())
+//                            done()
+//                        })
+//                    }
+//                }
+//            }
+//            
+//            it("Should return error when not logged in")
+//            {
+//                let sender = TestComponentsFactory.requestSender
+//                sender.logout()
+//                waitUntil(timeout: 10)
+//                {
+//                    done in
+//                    sender.send(PartnerApplicationsSearchRequest(query: self.query), withResponseHandler:PartnerApplicationsSearchResponseHandler()
+//                    {
+//                        (partnerApplications: Array<PartnerApplication>?, error: ErrorType?) -> Void in
+//                        expect(error).notTo(beNil())
+//                        expect(partnerApplications).to(beNil())
+//                        done()
+//                    })
+//                }
+//            }
+//        }
+//    }
 }
