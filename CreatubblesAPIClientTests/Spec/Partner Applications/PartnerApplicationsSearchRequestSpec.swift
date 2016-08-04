21f6c6cfa229819cdb2eeafdb8a856bb1e1fafab
Jakub Jankowski <kubajankowski90@gmail.com>
2016-08-04 14:51:48 +0200

    [CI-136] Handled Partner Applications Search endpoint, added tests.

---
+++ CreatubblesAPIClientTests/Spec/Partner Applications/PartnerApplicationsSearchRequestSpec.swift
@@ -0,0 +1,33 @@
+//
+//  PartnerApplicationsSearchRequestSpec.swift
+//  CreatubblesAPIClient
+//
+//  Created by Nomtek on 04.08.2016.
+//  Copyright © 2016 Nomtek. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import CreatubblesAPIClient
+
+class PartnerApplicationsSearchRequestSpec: QuickSpec
+{
+    let query = "example-app"
+    override func spec()
+    {
+        describe("Partner Applications Search Request")
+        {
+            it("Should have proper endpoint")
+            {
+                let request = PartnerApplicationsSearchRequest(query: self.query)
+                expect(request.endpoint).to(equal("partner_applications"))
+            }
+            
+            it("Should have proper method")
+            {
+                let request = PartnerApplicationsSearchRequest(query: self.query)
+                expect(request.method).to(equal(RequestMethod.GET))
+            }
+        }
+    }
+}
