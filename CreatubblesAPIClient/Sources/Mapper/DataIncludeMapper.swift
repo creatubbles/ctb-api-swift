c961d5bd42df8352a6aa6e66cb920091893ef4b0
Dawid Płatek <dawid@inspace.io>
2016-08-22 12:39:05 +0200

    CRI-356 - Fix an issue on parsing comment associated with notifications

--- CreatubblesAPIClient/Sources/Mapper/DataIncludeMapper.swift
+++ CreatubblesAPIClient/Sources/Mapper/DataIncludeMapper.swift
@@ -40,6 +40,7 @@ class DataIncludeMapper
         if let mapper = mapper as? UserMapper     { return User(mapper: mapper, dataMapper: self, metadata: metadata) as? T }
         if let mapper = mapper as? CreationMapper { return Creation(mapper: mapper, dataMapper: self, metadata: metadata) as? T }
         if let mapper = mapper as? GalleryMapper  { return Gallery(mapper:  mapper, dataMapper: self, metadata: metadata) as? T }
+        if let mapper = mapper as? CommentMapper  { return Comment(mapper:  mapper, dataMapper: self, metadata: metadata) as? T }
         if let mapper = mapper as? GallerySubmissionMapper { return GallerySubmission(mapper: mapper, dataMapper: self) as? T }
         if let mapper = mapper as? NotificationTextEntityMapper { return NotificationTextEntity(mapper: mapper, dataMapper: self) as? T }
         
