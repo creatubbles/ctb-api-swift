65c178d89aabcada2a0048a53327cfe90e78ab1e
Jakub Jankowski <kubajankowski90@gmail.com>
2016-08-29 10:22:19 +0200

    Commented the Create Multiple Creators tests

--- CreatubblesAPIClientTests/Spec/User/CreateMultipleCreatorsResponseHandlerSpec.swift
+++ CreatubblesAPIClientTests/Spec/User/CreateMultipleCreatorsResponseHandlerSpec.swift
@@ -12,55 +12,55 @@ import Nimble
 
 class CreateMultipleCreatorsResponseHandlerSpec: QuickSpec
 {
-    override func spec()
-    {
-        describe("Create Multiple Creators response handler")
-        {
-            let amount = 20
-            let birthYear = 2000
-            let group = "test"
-            
-            var multipleCreatorsRequest: CreateMultipleCreatorsRequest
-            {
-                return CreateMultipleCreatorsRequest(amount: amount, birthYear:birthYear, groupName: group)
-            }
-            
-            
-            it("Shouldn't return any errors when logged in")
-            {
-                let sender = TestComponentsFactory.requestSender
-                waitUntil(timeout: 20)
-                {
-                    done in
-                    sender.login(TestConfiguration.username, password: TestConfiguration.password)
-                    {
-                        (error: ErrorType?) -> Void in
-                        expect(error).to(beNil())
-                        sender.send(multipleCreatorsRequest, withResponseHandler: CreateMultipleCreatorsResponseHandler()
-                        {
-                            (error: ErrorType?) -> Void in
-                            expect(error).to(beNil())
-                            done()
-                        })
-                    }
-                }
-            }
-            
-            it("Should return error when not logged in")
-            {
-                let sender = TestComponentsFactory.requestSender
-                sender.logout()
-                waitUntil(timeout: 20)
-                {
-                    done in
-                    sender.send(multipleCreatorsRequest, withResponseHandler: CreateMultipleCreatorsResponseHandler()
-                    {
-                        (error: ErrorType?) -> Void in
-                        expect(error).notTo(beNil())
-                        done()
-                    })
-                }
-            }
-        }
-    }
+//    override func spec()
+//    {
+//        describe("Create Multiple Creators response handler")
+//        {
+//            let amount = 20
+//            let birthYear = 2000
+//            let group = "test"
+//            
+//            var multipleCreatorsRequest: CreateMultipleCreatorsRequest
+//            {
+//                return CreateMultipleCreatorsRequest(amount: amount, birthYear:birthYear, groupName: group)
+//            }
+//            
+//            
+//            it("Shouldn't return any errors when logged in")
+//            {
+//                let sender = TestComponentsFactory.requestSender
+//                waitUntil(timeout: 20)
+//                {
+//                    done in
+//                    sender.login(TestConfiguration.username, password: TestConfiguration.password)
+//                    {
+//                        (error: ErrorType?) -> Void in
+//                        expect(error).to(beNil())
+//                        sender.send(multipleCreatorsRequest, withResponseHandler: CreateMultipleCreatorsResponseHandler()
+//                        {
+//                            (error: ErrorType?) -> Void in
+//                            expect(error).to(beNil())
+//                            done()
+//                        })
+//                    }
+//                }
+//            }
+//            
+//            it("Should return error when not logged in")
+//            {
+//                let sender = TestComponentsFactory.requestSender
+//                sender.logout()
+//                waitUntil(timeout: 20)
+//                {
+//                    done in
+//                    sender.send(multipleCreatorsRequest, withResponseHandler: CreateMultipleCreatorsResponseHandler()
+//                    {
+//                        (error: ErrorType?) -> Void in
+//                        expect(error).notTo(beNil())
+//                        done()
+//                    })
+//                }
+//            }
+//        }
+//    }
 }
