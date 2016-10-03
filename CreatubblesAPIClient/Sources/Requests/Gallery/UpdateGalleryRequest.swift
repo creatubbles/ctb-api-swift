5299339fd0bf16cb64b0e614163669074a8bc422
Michal Miedlarz <m.miedlarz@nomtek.com>
2016-10-03 16:38:41 +0200

    Added openForAll field to UpdateGalleryData

--- CreatubblesAPIClient/Sources/Requests/Gallery/UpdateGalleryRequest.swift
+++ CreatubblesAPIClient/Sources/Requests/Gallery/UpdateGalleryRequest.swift
@@ -11,23 +11,30 @@ import UIKit
 class UpdateGalleryRequest: Request {
     override var method: RequestMethod   { return .PUT }
     override var endpoint: String { return "galleries/\(data.identifier)" }
-    override var parameters: Dictionary<String, AnyObject> {
-        var params = Dictionary<String, AnyObject>()
-        
-        if let name = data.name {
-            params["name"] = name
-        }
-        
-        if let galleryDescription = data.galleryDescription {
-            params["description"] = galleryDescription
-        }
-        
-        return params
-    }
-    
+    override var parameters: Dictionary<String, AnyObject> { return prepareParameters() }
     private let data: UpdateGalleryData
     
-    init(data: UpdateGalleryData) {
+    init(data: UpdateGalleryData)
+    {
         self.data = data
+    }
+
+    private func prepareParameters() -> Dictionary<String,AnyObject>
+    {
+        var params = Dictionary<String, AnyObject>()
+        
+        if let name = data.name
+        {
+            params["name"] = name
+        }
+        if let galleryDescription = data.galleryDescription
+        {
+            params["description"] = galleryDescription
+        }
+        if let openForAll = data.openForAll
+        {
+            params["open_for_all"] = openForAll
+        }
+        return params
     }
 }
