ba706c404373fb41af162a6d6fbf09d6666685a6
Michal Miedlarz <m.miedlarz@nomtek.com>
2017-03-09 14:50:19 +0100

    [CC-128] In progress

---
+++ CreatubblesAPIClientUnitTests/Spec/ErrorTransformerSpec.swift
@@ -0,0 +1,29 @@
+//
+//  ErrorTransformerSpec.swift
+//  CreatubblesAPIClient
+//
+//  Created by Michal Miedlarz on 09.03.2017.
+//  Copyright © 2017 Nomtek. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import CreatubblesAPIClient
+
+
+class ErrorTransformerSpec: QuickSpec
+{
+    let jsonAPIErrorJSON = ""
+    let serverErrorJSON  = ""
+    
+    override func spec()
+    {
+        describe("Error transformer")
+        {
+            it("Should parse JSONAPI Error")
+            {
+            }
+        }
+    }
+    
+}
