40a0f7c0b929c07e3fa305a71ccf194bef2eb3d2
Lorenzo Greco <lorenzo.gr90@gmail.com>
2018-02-15 16:00:56 +0100

    Reverted changes on pod files

--- Pods/CreatubblesAPIClient/CreatubblesAPIClient/Sources/Mapper/MetadataMapper.swift
+++ Pods/CreatubblesAPIClient/CreatubblesAPIClient/Sources/Mapper/MetadataMapper.swift
@@ -31,7 +31,6 @@ class MetadataMapper: Mappable {
     var bubbledUserIdentifiers: Array<String>?
     var bubbledGalleryIdentifiers: Array<String>?
     var abilityMappers: Array<AbilityMapper>?
-    var userFollowedHashtagsIdentifiers: Array<String>?
 
     var userFollowedUsersIdentifiers: Array<String>?
 
@@ -43,7 +42,7 @@ class MetadataMapper: Mappable {
         bubbledGalleryIdentifiers <- map["user_bubbled_galleries"]
 
         abilityMappers <- map["abilities"]
-        userFollowedHashtagsIdentifiers <- map["followed_hashtags"]
+        
         userFollowedUsersIdentifiers <- map["followed_users"]
     }
 }
